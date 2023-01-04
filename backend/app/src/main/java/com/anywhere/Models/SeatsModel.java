eab8f3b102c02ced6e566555c3fcf202fc17a40a
Avdhut Malankar <avdhut.satish@gmail.com>
2023-01-04 13:32:01 +0530

    removed unnessary packages

--- backend/app/src/main/java/com/anywhere/Models/SeatsModel.java
+++ backend/app/src/main/java/com/anywhere/Models/SeatsModel.java
@@ -1,6 +1,5 @@
 package com.anywhere.Models;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
--- backend/app/src/main/java/com/anywhere/servlets/Seats.java
+++ backend/app/src/main/java/com/anywhere/servlets/Seats.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
